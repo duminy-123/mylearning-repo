ca2104fd50afa76082b183a68605e168c6615457
duminy-123 <duminyk95@gmail.com>
2021-08-18 22:40:39 +0530

    second commit

---
+++ testCode1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import the Dataset
+df = pd.read_excel('Data.xlsx')
+df.head()
+df.info()
+
+plt.scatter(x=df['TotalOR1'], y=df['TotalOR2'])
+plt.title('The Relationship between OR Rates')
+plt.xlabel('OR Rate1')
+plt.ylabel('OR Rate2')
+plt.savefig('scatterplot.png')
+
+print("Hello")
---
+++ testCode2.py
@@ -0,0 +1,9 @@
+x = 0
+if (x < 1):
+    msg = "X is Less than One"
+else:
+    msg = "X is Greater than One"
+
+print(msg)
+
+- second change goes here
